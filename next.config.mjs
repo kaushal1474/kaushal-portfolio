4f7d13f6437566ee4d7c416265fd6c6be89fb52c
kaushal <kaushalchauhan096@gmail.com>
2024-12-14 18:50:29 +0530

    next 14 ssg

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: "out",
   output: "export",
-  crossOrigin: "anonymous",
   trailingSlash: true,
   images: { unoptimized: true },
 };
